fc6b85be19f0a054e584ec7b307164bb8e6a9b73
farfa <farfa7886@gmail.com>
2023-08-22 00:32:07 +0200

    pc

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -91,77 +91,102 @@ function togglePrettify(nochange) {
   <h1 class="w-full text-center font-bold text-4xl mt-10">
     Subway surfers JSON editor
   </h1>
-  <div
-    style="
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      margin-top: 30px;
-    "
-  >
-    <textarea
-      placeholder="Encrypted data"
-      class="textarea textarea-bordered textarea-lg"
-      style="width: 95%; height: 300px"
-      id="encryptedDataText"
-    ></textarea>
-  </div>
-  <div
-    style="
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      margin-top: 30px;
-    "
-  >
-    <button
-      class="btn btn-primary"
-      style="width: 95%"
-      @click="decrypt()"
-    >
-      Decrypt
-    </button>
+  <div class="grid grid-cols-1 gap-6 md:grid-cols-1 lg:grid-cols-2">
+    <div>
+      <div
+        style="
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          margin-top: 30px;
+        "
+      >
+        <textarea
+          placeholder="Encrypted data"
+          class="textarea textarea-bordered textarea-lg lg:mt-10 textAreaE"
+          style="width: 95%"
+          id="encryptedDataText"
+        ></textarea>
+      </div>
+      <div
+        style="
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          margin-top: 30px;
+        "
+      >
+        <button
+          class="btn btn-primary"
+          style="width: 95%"
+          @click="decrypt()"
+        >
+          Decrypt
+        </button>
+      </div>
+    </div>
+
+    <!-- DIVIDER -->
+    <div class="divider lg:hidden"></div>
+
+    <!-- END DIVIDER -->
+    <div>
+      <div style="width: 120px; margin-top: 30px; margin-left: 2.5%">
+        <div class="form-control">
+          <label class="label cursor-pointer">
+            <input
+              type="checkbox"
+              checked="checked"
+              class="checkbox checkbox-primary"
+              id="prettyChBox"
+              @change="togglePrettify()"
+            />
+            <span class="label-text">Pretty JSON</span>
+          </label>
+        </div>
+      </div>
+      <div style="width: 100%; display: flex; justify-content: center">
+        <textarea
+          placeholder="Decrypted data"
+          class="textarea textarea-bordered textarea-lg textAreaE"
+          style="width: 95%"
+          id="decryptedData-Text"
+        ></textarea>
+      </div>
+      <div
+        style="
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          margin-top: 30px;
+        "
+      >
+        <button
+          class="btn btn-secondary"
+          style="width: 95%"
+          @click="savei()"
+        >
+          Re-Encrypt and copy
+        </button>
+      </div>
+    </div>
   </div>
 
   <!-- DIVIDER -->
-
-  <div class="divider"></div>
-  <div style="width: 120px; margin-top: 30px; margin-left: 2.5%">
-    <div class="form-control">
-      <label class="label cursor-pointer">
-        <input
-          type="checkbox"
-          checked="checked"
-          class="checkbox checkbox-primary"
-          id="prettyChBox"
-          @change="togglePrettify()"
-        />
-        <span class="label-text">Pretty JSON</span>
-      </label>
-    </div>
-  </div>
-  <div style="width: 100%; display: flex; justify-content: center">
-    <textarea
-      placeholder="Decrypted data"
-      class="textarea textarea-bordered textarea-lg"
-      style="width: 95%; height: 300px"
-      id="decryptedData-Text"
-    ></textarea>
-  </div>
-  <div
-    style="
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      margin-top: 30px;
-    "
-  >
-    <button
-      class="btn btn-secondary"
-      style="width: 95%"
-      @click="savei()"
-    >
-      Re-Encrypt and copy
-    </button>
-  </div>
 </template>
+
+<style scoped>
+/* Mobile */
+@media only screen and (max-width: 1000px) {
+  .textAreaE {
+    height: 300px;
+  }
+}
+
+/* Desktop */
+@media only screen and (min-width: 1000px) {
+  .textAreaE {
+    height: 58vh;
+  }
+}
+</style>
