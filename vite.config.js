cfea68ce34e755695f3f852c8cc0196ee8a1ec8a
farfa <farfa7886@gmail.com>
2023-08-21 21:55:32 +0200

    uuuuh

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,24 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "icon.png", "icon.svg"],
+      manifest: {
+        name: "Subway surfers JSON editor",
+        short_name: "SS Decrypted",
+        description: "Subway surfers JSON editor",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
